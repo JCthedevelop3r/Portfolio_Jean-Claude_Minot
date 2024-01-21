fae351ae5b0173f3dd892b2bab25e133ea7d8431
JIICE <minotjeanclaude60@gmail.com>
2024-01-21 20:54:20 +0100

    Add basics HTML elements

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
-<template>
-  
-</template>
-
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+/*import { RouterLink, RouterView } from 'vue-router'*/
+
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
+import FooterComponent from '@/components/FooterComponent.vue'
+
+import BodyComponent from '@/components/BodyComponent.vue'
 
 </script>
+
+<template>
+    <HeaderComponent></HeaderComponent>
+    <BodyComponent></BodyComponent>
+    <FooterComponent></FooterComponent>
+
+
+</template>
 
 <style scoped>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <header>
+        <a href="#pdp"><img src="../assets/images/Pdp_moi.jpg" alt="Jeune homme brun au teint mate" id="pdp"/></a>
+
+        <nav>
+            <ul>
+                <a href="#presentation"><li>À propos de moi</li></a>
+                <a href="#"><li>CV</li></a>
+                <a href="#"><li>Cahier des charges</li></a>
+                <a href="#"><li>Espace commentaire</li></a>
+                <a href="#"><li>Me contacter</li></a>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+
+#pdp {
+
+height: 200px;
+
+}
+
+</style>
